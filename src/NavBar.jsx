d25cb9450b2e2b5a098e6427e9187a95fbdc5518
OlaiwonAbdullahi <132976400+OlaiwonAbdullahi@users.noreply.github.com>
2024-08-09 18:29:25 +0100

    new changes

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -3,23 +3,26 @@ import'./Navbar.css'
 function Navbar (){
 
   return(
-    <div>
+    <div className=' bg-slate-400 h-12 md:gap-32 md:px-80 px-5 gap-5 flex'>
       
         <div className="brandName mt-2 text-slate-800 text-2xl flex">
          NFTStore 
         </div>
-        <nav className=" bg-slate-400 h-12 md:gap-32 md:px-80 px-5 gap-5 flex">
-        <div className="Menu flex right-0 md:mt-2 mt-3 md:gap-5 gap-4 md:text-xl text-sm ml-4">
+        <nav>
+        <div className="Menu flex mx-auto right-0 md:mt-2 mt-3 md:gap-5 gap-4 md:text-xl text-sm ml-4">
+          <ul className='flex gap-4'>
             <li className='list-none p-0 m-0'><a href="#">Home</a></li>
             <li className='list-none p-0 m-0'><a href="#">About</a></li>
             <li className='list-none p-0 m-0'><a href="#">Shop</a></li>
             <li className='list-none p-0 m-0'><a href="#">Contact</a></li>
+            </ul>
         </div>
 
-        <div className='md:mt-2 mt-3 ml-8'>
-          <img src={Cart} className='text-slate-900 text-xl'></img>
-        </div>
+        
       </nav>
+      <div className='md:mt-2 mt-3 ml-8 '>
+          <img src={Cart} className='text-slate-900 h-10 w-11'></img>
+        </div>
     </div>
   )
 }
